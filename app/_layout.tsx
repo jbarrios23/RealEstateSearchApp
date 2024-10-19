5941553826bb4bcb2e310ddaf513fae807752edc
Javier Barrios <jbarrios.javier@gmail.com>
2024-10-19 11:11:59 -0400

    put navigation stuff

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,7 +3,9 @@ import { Stack } from "expo-router";
 export default function RootLayout() {
   return (
     <Stack>
-      <Stack.Screen name="index" />
+      <Stack.Screen name="index" options={{ title: 'Home' }} />
+      <Stack.Screen name="screen/map" options={{ title: 'Property Map' }} />
+      <Stack.Screen name="screen/property-details" options={{ title: 'Property Details' }} />
     </Stack>
   );
 }
